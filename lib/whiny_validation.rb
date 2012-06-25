833289b069b881cce895abb372f0d48f20ab7e99
Brian Morearty <brian@morearty.org>
2012-06-25 00:58:56 -0700

    Use ActiveSupport::LogSubscriber for colorized messages.
    If the app has turned off colorization it won't be colorized.

--- lib/whiny_validation.rb
+++ lib/whiny_validation.rb
@@ -8,9 +8,23 @@ module WhinyValidation
   end
 
   def whiny_validation
-    logger.debug { "Validation failed for #{inspect}:\n#{errors.full_messages.map{|m|"  #{m}"}.join("\n")}" }
+    ActiveSupport::Notifications.instrument("validation_failed.whiny_validation",
+                                            :object => self,
+                                            :error_messages => errors.full_messages)
+  end
+
+  class LogSubscriber < ActiveSupport::LogSubscriber
+    def validation_failed(event)
+      name = color("Validation failed", YELLOW, true)
+      object = event.payload[:object]
+      error_messages = color(event.payload[:error_messages].map{|m|"    => #{m}"}.join("\n"), YELLOW)
+
+      debug "  #{name}  #{object.inspect}\n#{error_messages}"
+    end
   end
 end
+
+WhinyValidation::LogSubscriber.attach_to :whiny_validation
 
 module ActiveRecord
   class Base
